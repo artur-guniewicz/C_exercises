4c5e724f42fbe9769908bc04da1e83f0fd774ec1
Artur <52582312+artur-guniewicz@users.noreply.github.com>
2020-04-25 22:57:49 +0200

    Add files via upload

---
+++ #basics_1/11. multidimensional arrays.c
@@ -0,0 +1,13 @@
+/* tablice wielowymiarowe */
+
+#include <stdio.h>
+
+int main()
+{
+    int t[10][20], p[4][3]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+
+    printf("p[1][2]=%i\n", p[1][2]);
+    printf("p[2][1]=%i\n", p[2][1]);
+
+    return 0;
+}
